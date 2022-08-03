a73fbe6ccebbc394426618b293bced6eb1d09e23
Chris Chen <chris.chen3128@gmail.com>
2022-08-02 21:09:37 -0400

    commit

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -7,11 +7,13 @@ module.exports = {
     name: "messageCreate",
     async execute(message, client) {
         if (message.mentions.members.size > 0) {
+            if (message.mentions.repliedUser) return;
             return user_ar(
                 message,
                 message.mentions.members,
                 await guild_fetch(message.guildId)
             );
+            r;
         }
     },
 };
