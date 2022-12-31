fa440e899ea1d182de1ceafdd11537d3a4065cb8
neriseo <82884420+Neiry6969@users.noreply.github.com>
2022-12-31 00:31:22 -0500

    Update randomrole.js

--- commands/roles/randomrole.js
+++ commands/roles/randomrole.js
@@ -1,195 +1,206 @@
+
+Search
+Pulls
+Issues
+Codespaces
+Marketplace
+Explore
+ 
+@Neiry6969 
+Neiry6969
+/
+dankexclusive
+Private
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+Settings
+dankexclusive/commands/roles/randomrole.js
+@Neiry6969
+Neiry6969 Update randomrole.js
+Latest commit c3dbe18 last week
+ History
+ 1 contributor
+177 lines (164 sloc)  6.86 KB
+
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { EmbedBuilder } = require("discord.js");
 
-const { discord_check_role } = require("../../utils/discord");
+const { guild_checkperm_mod } = require("../../utils/guild");
 const { error_reply } = require("../../utils/error");
+const { reference_letternumber } = require("../../utils/reference");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("randomrole")
-        .setDescription("Gives a role to random about of people")
+        .setDescription("gives a number of random people a role")
         .addStringOption((oi) => {
             return oi
                 .setName("quantity")
-                .setDescription("How many users will this role be added to.")
+                .setDescription(
+                    "A constant number: `123`, a short form: `2k`, a keyword: `max or half`"
+                )
                 .setRequired(true);
         })
         .addRoleOption((oi) => {
             return oi
                 .setName("role")
-                .setDescription("A role in the server.")
+                .setDescription("A valid role that exists in this server")
                 .setRequired(true);
         })
         .addRoleOption((oi) => {
             return oi
                 .setName("blacklist")
                 .setDescription(
-                    "A role in the server in which if a user has this role, they will not be reciveing the role."
+                    "A valid role that exists in this server that you want to blacklist"
                 );
         }),
 
     async execute(interaction, client) {
-        let error_message;
-        const checkAccess = await discord_check_role(interaction, [
-            "904456239415697441",
-        ]);
-        if (checkAccess === false) {
-            error_message = "You don't have the roles to use this command.";
-            return error_reply(interaction, error_message);
-        }
-
         const options = {
             quantity: interaction.options.getString("quantity"),
             role: interaction.options.getRole("role"),
             blacklist: interaction.options.getRole("blacklist"),
         };
+        requiredperms = [
+            "ManageChannels",
+            "ManageGuild",
+            "Administrator",
+            "ManageRoles",
+        ];
+        let message;
+        let pass = await guild_checkperm_mod(interaction, requiredperms);
 
-        const member_fetch = await interaction.guild.members.fetch();
-        const member_fetch_filterhumans = member_fetch.filter(
+        if (!pass === true) {
+            message = `\`You don't have the required permissions to preform this action\``;
+            return error_reply(interaction, message);
+        }
+        const fetchMembers = await interaction.guild.members.fetch();
+        const filter_members = fetchMembers.filter(
             (member) => !member.user.bot
         );
-        const users_hasrole = member_fetch_filterhumans.filter((member) =>
-            member.roles.cache.find((r) => r.id === options.role.id)
-        ).size;
-        let users_blacklisted = 0;
+        const noofusers = await reference_letternumber(
+            interaction,
+            options.quantity
+        );
+        if (noofusers > filter_members.size) {
+            message = `\`You can't give the role to more than the amount of members that the server has\n\`This server has \`${filter_members.size.toLocaleString()}\` members`;
+            return error_reply(interaction, message);
+        }
 
-        if (options.blacklist) {
-            if (options.role.id === options.blacklist.id) {
-                error_message = `Action role can't be the same as blacklist role!`;
-                return error_reply(interaction, error_message);
+        const role = options.role;
+        const role_blacklisted = options.blacklist;
+        if (role_blacklisted) {
+            if (role.id === role_blacklisted.id) {
+                message = `\`Action role can't be the same as blacklist role\``;
+                return error_reply(interaction, message);
             }
 
-            users_blacklisted = member_fetch_filterhumans.filter((member) =>
-                member.roles.cache.find((r) => r.id === options.blacklist.id)
+            const amount_blacklisted = fetchMembers.filter((member) =>
+                member.roles.cache.find((r) => r.id === role_blacklisted.id)
             ).size;
+            const amount_has_filter = fetchMembers.filter((member) =>
+                member.roles.cache.find((r) => r.id === role.id)
+            );
+
+            let amount_has = 0;
+            if (amount_has_filter) {
+                amount_has = fetchMembers.filter((member) =>
+                    member.roles.cache.find((r) => r.id === role.id)
+                ).size;
+            }
+
+            const avaliableadd =
+                filter_members.size - amount_blacklisted - amount_has;
+            if (noofusers > avaliableadd) {
+                message = `\`Quantity is higher than how many users in the server can give to\`\nThis server has \`${filter_members.size.toLocaleString()}\` members\nBlacklisted role: <@&${
+                    role_blacklisted.id
+                }> (${amount_blacklisted.toLocaleString()})\nRole: <@&${
+                    role.id
+                }> (${amount_has.toLocaleString()})\nAvaliable: \`${avaliableadd.toLocaleString()}\``;
+                return error_reply(interaction, message);
+            }
         }
 
-        const users_addallowed =
-            member_fetch_filterhumans.size - users_blacklisted - users_hasrole;
-        if (options.quantity > users_addallowed) {
-            error_message = `You can't give that role to that many users!\n\nServer Humans: \`${member_fetch_filterhumans.size.toLocaleString()}\`\n${
-                options.blacklist
-                    ? `Blacklisted role: ${
-                          options.blacklist
-                      } (${users_blacklisted.toLocaleString()})\n`
-                    : ""
-            }Role: ${
-                options.role
-            } (${users_hasrole.toLocaleString()})\nAvaliable: \`${users_addallowed.toLocaleString()}\``;
-            return error_reply(interaction, error_message);
-        }
+        already_added = [];
+        const embed = new EmbedBuilder()
+            .setColor("Random")
+            .setDescription(
+                `Fetching...\nGetting ready to set <@&${
+                    role.id
+                }> to \`${noofusers.toLocaleString()}\` random members in the server...`
+            );
 
-        const randomrole_message = await interaction.reply({
-            embeds: [
-                new EmbedBuilder().setDescription(
-                    `**Assign Random Role: IN PROCCESS**\n*Selecting random users to add role to...*`
-                ),
-            ],
+        const random_message = await interaction.reply({
+            embeds: [embed],
             fetchReply: true,
         });
 
-        const users_selected = [];
-        for (let i = 0; i < options.quantity; i++) {
-            if (options.blacklist) {
-                users_selected.push(
-                    member_fetch_filterhumans
-                        .filter(
-                            (member) =>
-                                !member.user.bot &&
-                                !member.roles.cache.find(
-                                    (r) => r.id === options.role.id
-                                ) &&
-                                !member.roles.cache.find(
-                                    (r) => r.id === options.blacklist.id
-                                )
-                        )
-                        .random()
+        let users = [];
+        for (let i = 0; i < noofusers; i++) {
+            let user;
+            if (role_blacklisted) {
+                user = fetchMembers
+                    .filter(
+                        (member) =>
+                            !member.user.bot &&
+                            !member.roles.cache.find((r) => r.id === role.id) &&
+                            !member.roles.cache.find(
+                                (r) => r.id === role_blacklisted.id
+                            )
+                    )
+                    .random().id;
+            } else {
+                user = fetchMembers
+                    .filter(
+                        (member) =>
+                            !member.user.bot &&
+                            !member.roles.cache.find((r) => r.id === role.id)
+                    )
+                    .random().id;
+            }
+            if (users.includes(user)) {
+                i = i - 1;
+            } else {
+                users.push(user);
+                const amountdone = i + 1;
+                const percentdont = ((amountdone / noofusers) * 100).toFixed(2);
+            }
+        }
+
+        let count = 0;
+        users.forEach((id) => {
+            interaction.guild.members.cache.get(id).roles.add(role);
+            count = count + 1;
+            const percentdont = ((count / noofusers) * 100).toFixed(2);
+            const remainderdevided = count % 5;
+
+            if (count === noofusers) {
+                embed.setDescription(
+                    `**Finished! YAY**\nThis command is avaliable again\n\nRole: <@&${
+                        role.id
+                    }>\n${
+                        role_blacklisted
+                            ? `Blacklisted Role: <@&${role_blacklisted.id}>\n`
+                            : ``
+                    }Amount Done: \`${count.toLocaleString()}/${noofusers.toLocaleString()}\` \`${percentdont}%\``
                 );
             } else {
-                users_selected.push(
-                    member_fetch_filterhumans
-                        .filter(
-                            (member) =>
-                                !member.user.bot &&
-                                !member.roles.cache.find(
-                                    (r) => r.id === options.role.id
-                                )
-                        )
-                        .random()
+                embed.setDescription(
+                    `**Adding roles...**\nPlease hold, don't run command again till finished processing\n\nRole: <@&${
+                        role.id
+                    }>\n${
+                        role_blacklisted
+                            ? `Blacklisted Role: <@&${role_blacklisted.id}>\n`
+                            : ``
+                    }Amount Done: \`${count.toLocaleString()}/${noofusers.toLocaleString()}\` \`${percentdont}%\``
                 );
             }
-        }
-
-        randomrole_message.edit({
-            embeds: [
-                new EmbedBuilder().setDescription(
-                    `**Assign Random Role: IN PROCCESS**\n*Adding role to selected users...*\n\nRole: ${
-                        options.role
-                    }\n${
-                        options.blacklist
-                            ? `\nBlacklist Role: ${options.blacklist}`
-                            : ""
-                    }Proccess: \`0/${users_selected.length.toLocaleString()}\``
-                ),
-            ],
+            return random_message.edit({ embeds: [embed] });
         });
-
-        let users_added = 0;
-        for (const user of users_selected) {
-            const user_addrole = await user.roles
-                .add(options.role)
-                .catch((error) => {
-                    randomrole_message.edit({
-                        embeds: [
-                            new EmbedBuilder().setDescription(
-                                `**Assign Random Role: FAILED**\n*An error occured...*\n\nRole: ${
-                                    options.role
-                                }\n${
-                                    options.blacklist
-                                        ? `\nBlacklist Role: ${options.blacklist}`
-                                        : ""
-                                }Proccess: \`${users_added.toLocaleString()}/${users_selected.length.toLocaleString()}\``
-                            ),
-                        ],
-                    });
-
-                    return false;
-                });
-
-            if (user_addrole === false) {
-                return;
-            }
-
-            users_added += 1;
-            randomrole_message.edit({
-                embeds: [
-                    new EmbedBuilder().setDescription(
-                        `**Assign Random Role: IN PROCCESS**\n*Adding role to selected users...*\n\nRole: ${
-                            options.role
-                        }\n${
-                            options.blacklist
-                                ? `\nBlacklist Role: ${options.blacklist}`
-                                : ""
-                        }Proccess: \`${users_added.toLocaleString()}/${users_selected.length.toLocaleString()}\``
-                    ),
-                ],
-            });
-        }
-
-        if (users_added === users_selected.length) {
-            randomrole_message.edit({
-                embeds: [
-                    new EmbedBuilder().setDescription(
-                        `**Assign Random Role: SUCCESS**\n*Finished adding the role to selected users.*\n\nRole: ${
-                            options.role
-                        }\n${
-                            options.blacklist
-                                ? `\nBlacklist Role: ${options.blacklist}`
-                                : ""
-                        }Completed: \`${users_added.toLocaleString()}/${users_selected.length.toLocaleString()}\``
-                    ),
-                ],
-            });
-        }
     },
 };
