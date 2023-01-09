042eb2ab235f1cfc31d7343e1f2824f171565a8a
neriseo <82884420+Neiry6969@users.noreply.github.com>
2023-01-08 21:02:37 -0500

    Update level.js

--- commands/level/level.js
+++ commands/level/level.js
@@ -27,7 +27,7 @@ module.exports = {
     cooldown: 10,
     async execute(interaction, client) {
         let error_message;
-        const INCREMENT = 60;
+        const INCREMENT = 50;
         let exp_cap;
 
         if (interaction.options.getSubcommand() === "show") {
