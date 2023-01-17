a8cc6b3e9c32b6dfd8141f17c504e7bce61a5377
neriseo <82884420+Neiry6969@users.noreply.github.com>
2023-01-17 09:57:37 -0500

    Update level.js

--- utils/level.js
+++ utils/level.js
@@ -9,7 +9,7 @@ const { EmbedBuilder } = require("discord.js");
 
 class Levelfunctions {
     static async user_exp_add_message(client, message) {
-        if(message.author.id === "685672031395905583") return; % blacklist poWer
+        if(message.author.id === "685672031395905583") return; // blacklist poWer
         const userData = await user_fetch(message.author.id);
         const guildData = await guild_fetch(message.guild.id);
         const level_initial = userData.levelInfo.level;
@@ -115,7 +115,7 @@ class Levelfunctions {
     }
 
     static async user_exp_add_interaction(client, message) {
-        if(message.interaction.user.id === "685672031395905583") return; % blacklist poWer
+        if(message.interaction.user.id === "685672031395905583") return; // blacklist poWer
         const user_discordData = await message.guild.members.fetch(
             message.interaction.user.id
         );
