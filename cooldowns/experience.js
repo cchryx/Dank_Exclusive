3737db2d3831101bfa0235828a4ea19c5aa72bee
Chris Chen <chris.chen3128@gmail.com>
2023-03-29 17:38:28 -0400

    commit

---
+++ cooldowns/experience.js
@@ -0,0 +1,33 @@
+// cooldowns.js
+
+const cooldowns = new Map();
+
+module.exports = {
+    setCooldown: function (userId, command, timeInSeconds) {
+        const key = `${userId}-${command}`;
+        if (cooldowns.has(key)) {
+            const expirationTime = cooldowns.get(key) + timeInSeconds * 1000;
+            const timeLeft = (expirationTime - Date.now()) / 1000;
+            return timeLeft;
+        } else {
+            cooldowns.set(key, Date.now());
+            return null;
+        }
+    },
+    checkCooldown: function (userId, command, timeInSeconds) {
+        const key = `${userId}-${command}`;
+        if (cooldowns.has(key)) {
+            const expirationTime = cooldowns.get(key) + timeInSeconds * 1000;
+            const timeLeft = (expirationTime - Date.now()) / 1000;
+            if (timeLeft > 0) {
+                return timeLeft;
+            } else {
+                cooldowns.set(key, Date.now());
+                return null;
+            }
+        } else {
+            cooldowns.set(key, Date.now());
+            return null;
+        }
+    },
+};
